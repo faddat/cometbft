ea9083d053543da30a1840dbfc1351dc897b8694
Jacob Gadikian <jacobgadikian@gmail.com>
2024-02-26 01:25:24 +0200

    send timeout of 5s

--- p2p/conn/connection.go
+++ p2p/conn/connection.go
@@ -42,7 +42,7 @@ const (
 	defaultRecvMessageCapacity = 22020096       // 21MB
 	defaultSendRate            = int64(5120000) // 5MB/s
 	defaultRecvRate            = int64(5120000) // 5MB/s
-	defaultSendTimeout         = 2 * time.Second
+	defaultSendTimeout         = 5 * time.Second
 	defaultPingInterval        = 60 * time.Second
 	defaultPongTimeout         = 45 * time.Second
 )
